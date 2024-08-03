d3f4897a64e729c1ff311da92a47764afa7c4d8d
unprosaiclabyrinth <hdongr2@uic.edu>
2024-08-03 05:48:35 +0000

    Make functions static

--- bblogger.c
+++ bblogger.c
@@ -7,12 +7,14 @@ static void event_exit(void);
     unsigned long code_vma_start_addr = 0;
 
 // callback function prototype for the basic block event
-dr_emit_flags_t event_app_instruction(void *drcontext, void *tag, instrlist_t *bb,
-                                      bool for_trace, bool translating);
+static dr_emit_flags_t
+event_app_instruction(void *drcontext, void *tag, instrlist_t *bb,
+                      bool for_trace, bool translating);
 
-void dr_client_main(__attribute__((unused)) client_id_t id,
-                    __attribute__((unused)) int argc,
-                    __attribute__((unused)) const char *argv[]) {
+DR_EXPORT void
+dr_client_main(__attribute__((unused)) client_id_t id,
+               __attribute__((unused)) int argc,
+               __attribute__((unused)) const char *argv[]) {
     dr_set_client_name("DrBblogger", "https://dynamorio.org");
 
     log_file = dr_open_file("bbtrace.log", DR_FILE_WRITE_OVERWRITE | DR_FILE_ALLOW_LARGE);
@@ -22,10 +24,11 @@ void dr_client_main(__attribute__((unused)) client_id_t id,
     dr_register_bb_event(event_app_instruction);
 }
 
-dr_emit_flags_t event_app_instruction(__attribute__((unused)) void *drcontext, void *tag,
-                                      __attribute__((unused)) instrlist_t *bb,
-                                      __attribute__((unused)) bool for_trace,
-                                      __attribute__((unused)) bool translating) {
+static dr_emit_flags_t
+event_app_instruction(__attribute__((unused)) void *drcontext, void *tag,
+                      __attribute__((unused)) instrlist_t *bb,
+                      __attribute__((unused)) bool for_trace,
+                      __attribute__((unused)) bool translating) {
     app_pc start_pc = dr_fragment_app_pc(tag);
     module_data_t *mod = dr_lookup_module(start_pc);
     char addr_str[64];
