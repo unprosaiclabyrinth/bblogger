b7647573fda0d45fb791cdab82dab9c0576c9f60
unprosaiclabyrinth <hdongr2@uic.edu>
2025-07-31 04:56:44 +0000

    Use after free = garbage bonkura

--- bblogger.c
+++ bblogger.c
@@ -6,28 +6,56 @@
 static file_t log_file;
 static void event_exit(void);
 
-static void trace_bb(char *bb, app_pc tag); // clean call @ BB entry
+static void trace_bb(app_pc tag); // clean call @ BB entry
 
 // callback function prototype for the basic block event
 static dr_emit_flags_t
 event_app_instruction(void *drcontext, void *tag, instrlist_t *bb,
                       bool for_trace, bool translating);
 
-DR_EXPORT void
-dr_client_main(__attribute__((unused)) client_id_t id,
-               __attribute__((unused)) int argc,
-               __attribute__((unused)) const char *argv[]) {
-    dr_set_client_name("DrBblogger", "https://dynamorio.org");
+#ifdef VVERBOSE
+typedef struct bb_data_t {
+    app_pc tag;
+    char *bbstr;
+    struct bb_data_t *next;
+} bb_data_t;
 
-    log_file = dr_open_file("bbtrace.log", DR_FILE_WRITE_OVERWRITE | DR_FILE_ALLOW_LARGE);
-    DR_ASSERT(log_file != INVALID_FILE);
+/* the linked list cache */
+static bb_data_t *bb_cache = NULL;
 
-    dr_register_exit_event(event_exit);
-    dr_register_bb_event(event_app_instruction);
+void cache_bb_string(app_pc tag, char *bbstr) {
+    bb_data_t *e = dr_global_alloc(sizeof(*e));
+    e->tag  = tag;
+    e->bbstr  = bbstr;
+    e->next = bb_cache;
+    bb_cache = e;
 }
 
+char *lookup_bb_string(app_pc tag) {
+    for (bb_data_t *e = bb_cache; e; e = e->next) {
+        if (e->tag == tag)
+            return e->bbstr;
+    }
+    return NULL;
+}
+
+void free_bb_cache(void)
+{
+    bb_data_t *e = bb_cache;
+    while (e != NULL) {
+        bb_data_t *next = e->next;
+        // free the disassembly string
+        dr_global_free(e->bbstr, strlen(e->bbstr) + 1);
+        // free the node itself
+        dr_global_free(e, sizeof(*e));
+        e = next;
+    }
+    bb_cache = NULL;
+}
+#endif /* VVERBOSE */
+
 // called at runtime, once per BB execution
-static void trace_bb(char *bb, app_pc tag) {
+static void trace_bb(app_pc tag) {
     app_pc app_pc = dr_fragment_app_pc(tag);
     module_data_t *mod = dr_lookup_module(app_pc);
     if (mod != NULL) {
@@ -37,8 +65,10 @@ static void trace_bb(char *bb, app_pc tag) {
 
         // Log according to verbosity level
         #ifdef VVERBOSE
-            dr_fprintf(log_file, "=== BB @ <%s> + %#lx ===\n%s\n", modulestr, modrel_pc, bb);
-            dr_global_free(bb, strlen(bb) + 1); 
+            char *bb = lookup_bb_string(tag);
+            if (bb) {
+                dr_fprintf(log_file, "=== BB @ <%s> + %#lx ===\n%s\n", modulestr, modrel_pc, bb);
+            }
         #else
             #ifdef VERBOSE
                 dr_fprintf(log_file, "<%s> + %#lx\n", modulestr, modrel_pc);
@@ -54,7 +84,6 @@ static dr_emit_flags_t
 event_app_instruction(void *drcontext, void *tag, instrlist_t *bb,
                       __attribute__((unused)) bool for_trace,
                       __attribute__((unused)) bool translating) {
-    char *heap_buf = NULL;
     #ifdef VVERBOSE
         // Disassemble the basic block into a local buffer
         char local[4096] = {0};
@@ -71,18 +100,35 @@ event_app_instruction(void *drcontext, void *tag, instrlist_t *bb,
         // Copy the local buffer into the DR heap for it to survive until the clean call
         *p = '\0'; // null-terminate the string
         size_t total_len = (p - local) + 1;
-        heap_buf = dr_global_alloc(total_len);
+        char *heap_buf = dr_global_alloc(total_len);
         memcpy(heap_buf, local, total_len);
+        cache_bb_string((app_pc)tag, heap_buf);
     #endif
 
     // insert a clean call at the top of the block
     dr_insert_clean_call(drcontext, bb, instrlist_first(bb),
-                         (void *)trace_bb, false, 2,
-                         OPND_CREATE_INTPTR((ptr_int_t)heap_buf),
+                         (void *)trace_bb, false, 1,
                          OPND_CREATE_INTPTR(tag));
     return DR_EMIT_DEFAULT;
 }
 
 static void event_exit(void) {
     dr_close_file(log_file);
+    #ifdef VVERBOSE
+        free_bb_cache();
+    #endif
+}
+
+/* Client entry point */
+DR_EXPORT void
+dr_client_main(__attribute__((unused)) client_id_t id,
+               __attribute__((unused)) int argc,
+               __attribute__((unused)) const char *argv[]) {
+    dr_set_client_name("DrBblogger", "https://dynamorio.org");
+
+    log_file = dr_open_file("bbtrace.log", DR_FILE_WRITE_OVERWRITE | DR_FILE_ALLOW_LARGE);
+    DR_ASSERT(log_file != INVALID_FILE);
+
+    dr_register_exit_event(event_exit);
+    dr_register_bb_event(event_app_instruction);
 }
